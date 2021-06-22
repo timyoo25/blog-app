c6947ea4f541588deb3eb04d800072d53be1ff0c
ANYTIME APPLIANCE <anytimeappliance@ANYTIMEs-Mac-mini.local>
2021-06-22 16:15:23 -0400

    fixed naming convention in db connection.js

--- db/connection.js
+++ db/connection.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 
 let MONGODB_URI =
   process.env.PROD_MONGODB ||
-  'mongodb://127.0.0.1:27017/productsAppDatabase'
+  'mongodb://127.0.0.1:27017/postsAppDatabase'
 
 // Uncomment to debug Mongoose queries
 // mongoose.set('debug', true)
