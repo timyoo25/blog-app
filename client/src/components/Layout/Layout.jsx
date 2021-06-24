384c5ae106e23394fae18e0ba4f90e7d58fe3557
ANYTIME APPLIANCE <anytimeappliance@ANYTIMEs-Mac-mini.local>
2021-06-24 11:25:55 -0400

    minor CSS touches, added a layout to Layout and implemented it into the Posts.jsx file. created skeleton for Nav and Footer

---
+++ client/src/components/Layout/Layout.jsx
@@ -0,0 +1,13 @@
+import "./Layout.css";
+import Nav from "../Nav/Nav";
+import Footer from "../Footer/Footer";
+
+const Layout = (props) => (
+  <div className="layout">
+    <Nav />
+    <div className="layout-children">{props.children}</div>
+    <Footer />
+  </div>
+);
+
+export default Layout;
