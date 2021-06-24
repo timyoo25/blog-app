eb09c7371e5ef9d5fd3f86466fbb2ff9488269cb
Will Metivier <willnolin@gmail.com>
2021-06-24 11:23:50 -0400

    built CreatePost form, style, add Route path=/new to App.js line 17

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import Posts from './screens/Posts/Posts'
+import PostCreate from './screens/PostCreate/PostCreate'
 import { Route } from 'react-router-dom'
 
 import './App.css';
@@ -7,13 +8,15 @@ function App() {
   return (
     <div className="App">
   
-      <Route path="/">
+      <Route exact path="/">
         <Posts />
       </Route>
       <Route path="">
         
       </Route>
-     
+      <Route path="/new">
+        <PostCreate />
+     </Route>
     </div>
   );
 }
