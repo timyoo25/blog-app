abc53301bac69688b357072be765d92be6871456
gchar031 <43050919+gchar031@users.noreply.github.com>
2021-06-24 11:26:46 -0400

    detail completed and edit btn added

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,17 @@
-import Posts from './screens/Posts/Posts'
-import { Route } from 'react-router-dom'
+import Posts from "./screens/Posts/Posts";
+import { Route } from "react-router-dom";
 
-import './App.css';
+import PostDetail from "./screens/PostDetail/PostDetail";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-  
       <Route path="/">
         <Posts />
       </Route>
-      <Route path="">
-        
-      </Route>
-     
+      <Route path=""></Route>
+      <Route path="/posts/:id" component={PostDetail} />
     </div>
   );
 }
---
+++ client/src/screens/PostDetail/PostDetail.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+import { getPost } from "../../services/posts.js";
+import { useParams } from "react-router";
+import { Link } from "react-router-dom";
+import Layout from "../../components/Layout/Layout";
+
+export default function PostDetail() {
+  const [post, setPost] = useState([]);
+  const { id } = useParams();
+  //grab post details
+  useEffect(() => {
+    async function fetchPost() {
+      const res = await getPost(id);
+      setPost(res);
+    }
+    fetchPost();
+  }, []);
+
+  const createdDate = new Date(post.createdAt);
+  return (
+    // <Layout>
+    <div className="post-detail" key={id}>
+      <h4 className="post-name">{post.name}</h4>
+      <p className="post-comment">{post.comment}</p>
+      <p className="timestamp">{createdDate.toDateString()}</p>
+      <Link to={`/posts/${id}/edit`}>
+        <button className="btn-edit">Edit</button>
+      </Link>
+    </div>
+    // </Layout>
+  );
+}
